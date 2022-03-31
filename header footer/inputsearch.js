9573f10ad4954f42e7c0f574d6a14fde227eadd7
Bhushan Wagh <99672577+Bhushan-Wagh98@users.noreply.github.com>
2022-03-31 13:29:47 +0530

    header is created

---
+++ header footer/inputsearch.js
@@ -0,0 +1,239 @@
+const inputsearch=async()=>
+{
+    try
+    {
+        let input=document.getElementById("navbarsearch").value;
+        input=input.trim().split(" ");
+        for(let i=0; i<input.length; i++)
+        {
+            input[i]=input[i].toLowerCase();
+        }
+        
+        //console.log(input);
+
+        let childcare=await fetch(`../API's/child care.json`)
+        let childcaredata=await childcare.json();
+        let childarr=childcaredata.productList;
+        //console.log('childarr:', childarr)
+
+        let food=await fetch(`../API's/food and drinks.json`);
+        let fooddata=await food.json();
+        let foodarr=fooddata.productList;
+        //console.log('foodarr:', foodarr)
+
+        let personalcare=await fetch(`../API's/personal care.json`);
+        let personaldata=await personalcare.json();
+        let personalarr=personaldata.productList;
+        //console.log('personalarr:', personalarr)
+
+        let protein=await fetch(`../API's/protein and fitness.json`);
+        let proteindata=await protein.json();
+        let proteinarr=proteindata.productList;
+        //console.log('proteinarr:', proteinarr)
+
+        let swellness=await fetch(`../API's/sexual wellness.json`);
+        let swellnessdata=await swellness.json();
+        let swellnessarr=swellnessdata.productList;
+        //console.log('swellnessarr:', swellnessarr)
+
+        let skincare=await fetch(`../API's/skin and hair care.json`);
+        let skincaredata=await skincare.json();
+        let skincarearr=skincaredata.productList;
+        //console.log('skincarearr:', skincarearr)
+
+        let vitamins=await fetch(`../API's/vitamins and suppliments.json`);
+        let vitaminsdata=await vitamins.json();
+        let vitaminsarr=vitaminsdata.productList;
+        //console.log('vitaminsarr:', vitaminsarr)
+
+        let weight=await fetch(`../API's/weight management.json`);
+        let weightdata=await weight.json();
+        let weightarr=weightdata.productList;
+        //console.log('weightarr:', weightarr)
+
+
+        let defaultprod=[];
+        for(let i=0; i<skincarearr.length; i++)
+        {
+            if(skincarearr[i].outOfStock===true && skincarearr[i].productName=="Auggmin Herbal Vibe Hair Growth Kit - 3 in 1 Pack")
+            {
+                defaultprod.push(skincarearr[i]);
+                break;
+            }
+        }
+
+        for(let j=0; j<foodarr.length; j++)
+        {
+            if(foodarr[j].outOfStock===true && foodarr[j].productName=="15 Days Weight Loss Pack (Meal Shake (Chocolate) + Meal-Soup + Green Coffee + Green Tea)")
+            {
+                defaultprod.push(foodarr[j]);
+                break;
+            }
+        }
+
+
+        let arr=[];
+        pushdata(arr, childarr);
+        pushdata(arr, foodarr);
+        pushdata(arr, personalarr);
+        pushdata(arr, proteinarr);
+        pushdata(arr, swellnessarr);
+        pushdata(arr, skincarearr);
+        pushdata(arr, vitaminsarr);
+        pushdata(arr, weightarr);
+        //console.log(arr);
+
+        let bag=[];
+        for(let i=0; i<arr.length; i++)
+        {
+            let flag1=false;
+            let flag2=false;
+
+            let name=arr[i].productName.trim().split(" ");
+
+            if(input.length>1)
+            {
+                for(let k=0; k<input.length; k++)
+                {
+                    for(let j=0; j<name.length; j++)
+                    {
+                        name[j]=name[j].toLowerCase();
+
+                        if(input[0]===name[j])
+                        {
+                            //console.log(name[j], input[k]);
+                            flag1=true;
+                        }
+
+                        if(input[1]===name[j] && flag1===true)
+                        {
+                            //console.log(name[j], input[k]);
+                            flag2=true;
+                            break;
+                        }
+                    }
+
+                    if(flag2===true)
+                    {
+                        break;
+                    }
+                }
+
+                if(flag2===true)
+                {
+                    bag.push(arr[i]);
+                }
+            }
+            else
+            {
+                for(let k=0; k<input.length; k++)
+                {
+                    for(let j=0; j<name.length; j++)
+                    {
+                        name[j]=name[j].toLowerCase();
+
+                        if(input[k]===name[j])
+                        {
+                            //console.log(name[j], input[k]);
+                            flag1=true;
+                        }
+                    }
+
+                    if(flag1===true)
+                    {
+                        break;
+                    }
+                }
+
+                if(flag1===true)
+                {
+                    bag.push(arr[i]);
+                }
+            }
+        }
+        inputonfocus(bag, input, defaultprod);
+    }
+
+
+    catch(err)
+    {
+        console.log(err);
+    }
+}
+
+
+
+function pushdata(arr, arr2)
+{
+    for(let i=0; i<arr2.length; i++)
+    {
+        arr.push(arr2[i]);
+    }
+}
+
+
+const inputonfocus=(data, input, defaultprod)=>
+{
+    let focusdiv=document.getElementById("onfocuscontent");
+    focusdiv.innerHTML=null;
+    //console.log(data);
+
+    if(data===undefined || input=="")
+    {
+        let focusdiv=document.getElementById("onfocuscontent");
+        focusdiv.innerHTML=null;
+
+        let h4=document.createElement("h4");
+        h4.innerText="Hot Offers";
+        focusdiv.append(h4);
+
+        defaultprod.forEach(function(ele)
+        {
+            let focusdiv=document.getElementById("onfocuscontent");
+            let div=document.createElement("div");
+
+            let img=document.createElement("img");
+            img.src=ele.media[0].absoluteMediaPath;
+
+            let title=document.createElement("p");
+            title.innerText=ele.productName;
+
+            div.append(img, title);
+            focusdiv.append(div);
+        })
+    }
+    else if(data.length==0)
+    {
+        let p=document.createElement("p");
+        p.innerText="No results found";
+
+        focusdiv.append(p);
+    }
+    else if(data.length>0)
+    {
+        data.forEach(function(ele, ind)
+        {
+            if(ind<6)
+            {
+                let focusdiv=document.getElementById("onfocuscontent");
+                let div=document.createElement("div");
+
+                let img=document.createElement("img");
+                img.src=ele.media[0].absoluteMediaPath;
+
+                let title=document.createElement("p");
+                title.innerText=ele.productName;
+
+                div.append(img, title);
+                focusdiv.append(div);
+            }
+        })
+    }
+
+}
+
+const inputonfocusout=()=>
+{
+    let focusdiv=document.getElementById("onfocuscontent");
+    focusdiv.innerHTML=null;
+}
---
+++ header footer/shoppingheader.js
@@ -0,0 +1,87 @@
+function shoppingheader()
+{
+    return `<div id="navbar">
+        
+    <div id="imagelogo">
+        <img style="height: 36px;" src="https://www.lybrate.com/img/Lybrate-Goodkart-Logo.png" alt="GoodKart">
+    </div>
+
+    <div id="navb-arsearch-div">
+        <input type="text" id="navbarsearch" placeholder="Search for health products" onfocus="inputsearch()" onfocusout="inputonfocusout()" oninput="inputsearch()">
+        <button onclick="inputsearch()">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"></path></svg>
+        </button>
+        <div id="onfocuscontent"></div>
+    </div>
+    
+    <div id="left-itemsi-in-navbar">
+        
+        <div class="sign-in-drop" onmouseleave="clear_dropdown()" onmouseover="dropdown()">
+            <p>Hello, Sign in</p>
+            <svg xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>
+        </div>
+        
+        <div id="top-dropdown" onmouseover="dropdown()" onmouseleave="clear_dropdown()" onscroll="clear_dropdown()"></div>
+    
+        <div class="lybrateCash">
+            <img style="margin-right: 4px;" src="https://assets.lybrate.com/q_auto,f_auto,h_20,w_20/imgs/product/d2d/PatientGoldMembership/lybrate_icon_red_large.png" alt="lybrateCash">
+            LybrateCash
+        </div>
+
+        <div>
+            <button>Home</button>
+        </div>
+
+        <div style="display: flex; align-items: center; cursor: pointer;">
+            <p id="cart-count">0</p>
+            <svg fill="rgb(200,37,6)" xmlns="http://www.w3.org/2000/svg" height="30px" width="30px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M8,17.816a2.3,2.3,0,0,1-2.178-1.728L3.411,5.261a2.087,2.087,0,0,0-.695-1.006L0.475,2.771a0.815,0.815,0,0,1-.227-1.14L0.364,1.46a0.839,0.839,0,0,1,1.155-.224L3.76,2.721A3.89,3.89,0,0,1,5.24,4.864L7.654,15.691A0.434,0.434,0,0,0,8,15.968H19.965a0.488,0.488,0,0,0,.386-0.3l1.728-6.789a0.833,0.833,0,0,1,1.01-.6l0.2,0.05a0.82,0.82,0,0,1,.6,1l-1.728,6.789a2.32,2.32,0,0,1-2.2,1.694H8Zm11.754,2.695A1.534,1.534,0,1,1,18.223,19,1.525,1.525,0,0,1,19.757,20.511Zm-8.842,0A1.535,1.535,0,1,1,9.381,19,1.525,1.525,0,0,1,10.916,20.511Z"></path></svg>
+        </div>
+    
+    </div>
+
+    
+</div>
+<div id="category">
+    <div style="display: flex; margin: auto; width: 90%; justify-content: space-between; align-items: center;">
+        <div style="cursor:context-menu;" onscroll="clear_dropdown()" onmouseleave="clear_dropdown()" onmouseover="shopbycategory()">
+            <p>Shop By Category</p>
+            <svg fill="white" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>
+        </div>
+        <div id="shop-by-category-drop" style="display: inline;" onscroll="clear_dropdown()" onmouseover="shopbycategory()" onmouseleave="clear_dropdown()"></div>
+
+        <div style="cursor:context-menu;" onmouseover="clear_dropdown()" onscroll="clear_dropdown()" onmouseleave="clear_dropdown()">Redeem LybrateCash</div>
+
+        <div style="cursor:context-menu;" onmouseover="swellnessfn()" onscroll="clear_dropdown()" onmouseleave="clear_dropdown()">
+            <p>Sexual Wellness</p>
+            <svg fill="white" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>
+        </div>
+        <div id="swellness-drop" style="display: inline;" onscroll="clear_dropdown()" onmouseover="swellnessfn()" onmouseleave="clear_dropdown()"></div>
+
+        <div style="cursor:context-menu;" onmouseover="fooddrink()" onmouseleave="clear_dropdown()" onscroll="clear_dropdown()">
+            <p>Food & Drink</p>
+            <svg fill="white" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>
+        </div>
+        <div id="food-drop" style="display: inline;" onscroll="clear_dropdown()" onmouseover="fooddrink()" onmouseleave="clear_dropdown()"></div>
+
+        <div style="cursor:context-menu;" onmouseover="vitaminsfn()" onmouseleave="clear_dropdown()" onscroll="clear_dropdown()">
+            <p>Vitamin & Supplement</p>
+            <svg fill="white" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>
+        </div>
+        <div id="vitamis-drop" style="display: inline;" onmouseover="vitaminsfn()" onmouseleave="clear_dropdown()" onscroll="clear_dropdown()"></div>
+
+        <div style="cursor:context-menu;" onmouseover="weightdropfn()" onmouseleave="clear_dropdown()" onscroll="clear_dropdown()">
+            <p>Weight Management</p>
+            <svg fill="white" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>
+        </div>
+        <div id="weight-drop" style="display: inline;" onmouseover="weightdropfn()" onmouseleave="clear_dropdown()" onscroll="clear_dropdown()"></div>
+
+        <div style="cursor:context-menu;" onmouseover="childdropfn()" onmouseleave="clear_dropdown()" onscroll="clear_dropdown()">
+            <p>Child Care</p>
+            <svg fill="white" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path ng-attr-d="{{$root.icons[ctrl.p]}}" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"></path></svg>
+        </div>
+        <div id="child-drop" style="display: inline;" onmouseover="childdropfn()" onmouseleave="clear_dropdown()" onscroll="clear_dropdown()"></div>
+    </div>
+</div>`;
+}
+
+export default shoppingheader;
